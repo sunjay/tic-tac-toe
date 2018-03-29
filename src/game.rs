186f6da3ce0d5b3aba334be29fa18d5e03fbea51
Sunjay Varma <varma.sunjay@gmail.com>
2018-03-29 11:13:59 -0400

    Converted test-input folder into actual tests

--- src/game.rs
+++ src/game.rs
@@ -328,3 +328,92 @@ impl Game {
         &self.tiles
     }
 }
+
+// These are tests! Rust has testing built-in so you get a streamlined experience that encourages
+// you to write tests more often.
+// To run these tests, run `cargo test`
+// More information: https://doc.rust-lang.org/book/second-edition/ch11-00-testing.html
+
+// #[cfg(test)] tells the compiler to only include the rest of this code if we are running
+// `cargo test`. This speeds up compile times since Rust doesn't need to process a bunch of code
+// which won't be run otherwise.
+#[cfg(test)]
+mod tests {
+    // This imports everything above so we get access to our Game, Winner, Piece, etc. types
+    use super::*;
+
+    //TODO: HELP WANTED. Please help us by writing more tests. These are not even close to
+    // exhaustive, but they are a good start!
+
+    // This is a test! Just add #[test] to a regular function and Rust will find that function
+    // and run it during `cargo test`
+    #[test]
+    fn col_3_o_wins() {
+        // To test the game, we just have to create a game, play on it a bit, and then check
+        // what happened
+        let mut game = Game::new();
+        // In tests, it is okay to "unwrap" and ignore errors. If something goes wrong, the test
+        // will fail because unwrap will exit with an error
+        // It can be helpful to use "expect" instead since with that you can provide a message
+        // for more context.
+        game.make_move(0, 0).unwrap();
+        game.make_move(2, 2).unwrap();
+        game.make_move(2, 1).unwrap();
+        game.make_move(1, 2).unwrap();
+        game.make_move(0, 1).unwrap();
+        game.make_move(0, 2).unwrap();
+        // assert_eq! is a special macro (like `println!`) which checks if two things are equal
+        // and then exits with an error if they are not. We use this to make sure game behaves
+        // as we expect
+        assert_eq!(game.winner().unwrap(), Winner::O);
+    }
+
+    #[test]
+    fn diag_1_x_wins() {
+        let mut game = Game::new();
+        game.make_move(0, 0).unwrap();
+        game.make_move(0, 1).unwrap();
+        game.make_move(2, 2).unwrap();
+        game.make_move(2, 1).unwrap();
+        game.make_move(1, 1).unwrap();
+        assert_eq!(game.winner().unwrap(), Winner::X);
+    }
+
+    #[test]
+    fn diag_2_x_wins() {
+        let mut game = Game::new();
+        game.make_move(0, 2).unwrap();
+        game.make_move(0, 1).unwrap();
+        game.make_move(2, 0).unwrap();
+        game.make_move(2, 1).unwrap();
+        game.make_move(1, 1).unwrap();
+        assert_eq!(game.winner().unwrap(), Winner::X);
+    }
+
+    #[test]
+    fn row_2_o_wins() {
+        let mut game = Game::new();
+        game.make_move(0, 0).unwrap();
+        game.make_move(1, 0).unwrap();
+        game.make_move(2, 1).unwrap();
+        game.make_move(1, 1).unwrap();
+        game.make_move(0, 2).unwrap();
+        game.make_move(1, 2).unwrap();
+        assert_eq!(game.winner().unwrap(), Winner::O);
+    }
+
+    #[test]
+    fn tie() {
+        let mut game = Game::new();
+        game.make_move(0, 0).unwrap();
+        game.make_move(0, 1).unwrap();
+        game.make_move(0, 2).unwrap();
+        game.make_move(2, 0).unwrap();
+        game.make_move(2, 1).unwrap();
+        game.make_move(2, 2).unwrap();
+        game.make_move(1, 0).unwrap();
+        game.make_move(1, 2).unwrap();
+        game.make_move(1, 1).unwrap();
+        assert_eq!(game.winner().unwrap(), Winner::Tie);
+    }
+}
